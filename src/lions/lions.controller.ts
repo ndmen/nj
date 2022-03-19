a5c81c27ac366bc878b9b343923146dfe2854834
Artem Abdullin <artemleelooai@gmail.com>
2022-03-19 22:31:17 +0200

    Add Get/Redirect and Get/:id Controllers

--- src/lions/lions.controller.ts
+++ src/lions/lions.controller.ts
@@ -1,10 +1,25 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Query, Redirect, Param } from '@nestjs/common';
 
 @Controller('lions')
   export class lionsController {
     @Get()
     findAll(): string {
         return 'Get all lions';
+    }
+
+    @Get(':id') 
+        findOne(@Param() params) : string {
+            console.log(params.id);
+            return `Get a lion by id ${params.id}`;
+    }
+    
+    //Redirect response on address (Ask)
+    @Get('blog')
+    @Redirect('http://blog.localhost:3000', 302)
+    getBlog(@Query('version') version ) {
+        if (version && version === 5) {
+            return { url: 'http://blog.localhost:3000/v5' }; 
+        }
     }
 
     @Post()
--- src/owls/owls.controller.ts
+++ src/owls/owls.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { create } from 'domain';
+import { Controller, Get, Post, Query, Redirect, Param } from '@nestjs/common';
 
 @Controller('owls')
   export class OwlsController {
     @Get()
     findAll(): string {
         return 'Get all owls';
+    }
+
+    @Get(':id')
+    findOne(@Param() params) : string {
+        console.log(params.id);
+        return `Get a owl by id ${params.id}`;
+    }
+
+    //Redirect responce on address (Ask)
+    @Get('blog')
+    @Redirect('http://blog.localhost:3000', 302)
+    getBlog(@Query('version') version) {
+        if (version && version === 5) {
+            return { url: 'http://blog.localhost:3000/v5' };
+        }
     }
 
     @Post()
