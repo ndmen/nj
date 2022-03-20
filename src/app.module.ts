8beb10fba0870cbc605a45b68cf0585be2d847dd
Artem Abdullin <artem.abdullin@anoda.mobi>
2022-03-20 11:41:17 +0200

    Add Controller to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { TigersModule } from './tigers/tigers.module';
 import { CatsModule } from './cats/cats.module';
 import { DogsModule } from './dogs/dogs.module';
+import { lionsController } from './lions/lions.controller';
+import { OwlsController } from './owls/owls.controller';
 
 @Module({
   imports: [ DogsModule, TigersModule, CatsModule],
-  controllers: [AppController],
+  controllers: [AppController, OwlsController, lionsController],
   providers: [AppService],
 })
 export class AppModule {}
